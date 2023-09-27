616a25a1b302971c243d2676dc21978a4ec55ac2
Kepsondiaz <kherabadiaby328@gmail.com>
2023-09-27 13:21:32 +0000

    fixe syntax error

---
+++ alal/__init__.py
@@ -0,0 +1,6 @@
+from alal.cards.service import Card
+from alal.cardUsers.service import CardUser
+from alal.disputes.service import Dispute
+from alal.Transactions.service import Transaction
+from alal.base import webhook_authentification
+from alal import model
